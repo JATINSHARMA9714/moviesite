fda0148fe26ad6d7c1b502cccc627b7502cbf9c8
unknown <jatinsharma6204063696@gmail.com>
2023-05-10 20:07:21 +0530

    moviesite made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import MovieItems from './components/MovieItems';
+import Navbar from './components/Navbar';
+import Movie from './components/Movie';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Movie/>
+    </>
   );
 }
 
